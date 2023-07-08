df891c74a5db02c34ffe3620de9c66aee11721a2
shinopai <harbor.shinozaki@gmail.com>
2023-07-08 23:58:03 +0000

    認証機能導入

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
+  # 認証ルート
+  devise_for :users, skip: 'sessions', controllers: {
+        sessions: 'users/sessions',
+        registrations: 'users/registrations'
+      }
+  devise_scope :user do
+    get	'/users/sign_in', to: 'users/sessions#new', as: :new_user_session
+    post	'/users/sign_in', to: 'users/sessions#create', as: :user_session
+    get	'/users/sign_out', to: 'users/sessions#destroy', as: :destroy_user_session
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
