2d68f901358e20d247e8eda7f10e8eb75541ffc6
shinopai <harbor.shinozaki@gmail.com>
2023-07-25 06:24:17 +0000

    発注先テストデータ作成

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,14 +12,21 @@
 # )
 # end
 
-require 'securerandom'
+# require 'securerandom'
 
-# 在庫商品テストデータ
-3500.times do
-Item.create(
-  name: Faker::Commerce.product_name,
-  price: Faker::Number.between(from: 300, to: 99999),
-  code: '83' + SecureRandom.alphanumeric(4),
-  stock: rand(0..100)
+# # 在庫商品テストデータ
+# 3500.times do
+# Item.create(
+#   name: Faker::Commerce.product_name,
+#   price: Faker::Number.between(from: 300, to: 99999),
+#   code: '83' + SecureRandom.alphanumeric(4),
+#   stock: rand(0..100)
+# )
+# end
+
+# 発注先テストデータ
+100.times do
+Supplier.create(
+  name: Faker::Company.name
 )
 end
